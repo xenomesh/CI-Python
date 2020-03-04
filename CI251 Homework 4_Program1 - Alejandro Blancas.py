8e9d13545a8183009779b7dd1a7c5ac84bbea081
xenomesh <60243236+xenomesh@users.noreply.github.com>
2020-03-04 13:47:51 -0600

    Update CI251 Homework 4_Program1 - Alejandro Blancas.py

--- CI251 Homework 4_Program1 - Alejandro Blancas.py
+++ CI251 Homework 4_Program1 - Alejandro Blancas.py
@@ -1,8 +1,8 @@
 # Alejandro Blancas
 # CI251
 # Homework 4 - Using Dictonaries
-#There are two files coside with this program
-#menu_choices.py & save_load_dict.py
+# There are two files that needed for this program to work:
+# menu_choices.py & save_load_dict.py
 import menu_choices
 import save_load_dict
     #Global constants for menu choices
