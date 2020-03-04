dbae96c2d24a66c8c2c5f23b154179c9bf7bfc09
xenomesh <60243236+xenomesh@users.noreply.github.com>
2020-03-04 13:39:22 -0600

    Add files via upload

---
+++ save_load_dict.py
@@ -0,0 +1,19 @@
+import pickle
+
+def pickle_it(contacts):
+    output_file = open('contacts.dat', 'wb')
+    pickle.dump(contacts, output_file)
+    output_file.close()
+
+def unpickle_it():
+    input_file = open('contacts.dat', 'rb')
+    # Load the contact dictionary or create an empty dictionary if file is empty
+    try:
+        contacts = pickle.load(input_file)
+    except:
+        contacts = {}
+
+    # Close the file
+    input_file.close()
+    # Return the contacts dictionary
+    return contacts
